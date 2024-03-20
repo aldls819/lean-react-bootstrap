4b5816f00ca7120d47e06b8ac92a4ad342945ff6
aldls819 <aldls819@gmail.com>
2024-03-20 14:00:07 +0900

    :lipstick: Design: Bootstrap 사용
    Bootstrap 사용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "react-bootstrap/Accordion";
+
+const data = [
+  {
+    id: 1,
+    name: "텍스트1",
+    content: "안녕하시렵니까?",
+  },
+  {
+    id: 2,
+    name: "텍스트2",
+    content: "안녕하시렵니까?",
+  },
+  {
+    id: 3,
+    name: "텍스트3",
+    content: "안녕하시렵니까?",
+  },
+];
 
 function App() {
   return (
     <div className="App">
+      {data.map((e, i) => (
+        <Accordion defaultActiveKey="0">
+          <Accordion.Item eventKey={i}>
+            <Accordion.Header>{e.name}</Accordion.Header>
+            <Accordion.Body>{e.content}</Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      ))}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
