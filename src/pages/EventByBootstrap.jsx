4590d49bd2aa5b9849a7938a8b6c71576d2a5e10
aldls819 <aldls819@gmail.com>
2024-03-21 16:58:02 +0900

    :sparkles: Feat: 메인 화면 구현
    메인 화면 구현

--- src/pages/EventByBootstrap.jsx
+++ src/pages/EventByBootstrap.jsx
@@ -87,12 +87,37 @@ const EventByBootstrap = () => {
 
               <div className={styles.top__sales_list}>
                 {productList.map((e, i) => (
-                  <Card style={{}}>
+                  <Card style={{ minWidth: 240, borderRadius: 16 }}>
                     <Card.Img src={e.img} className={styles.product__image} />
+                    <Card.Body style={{ padding: 0 }}>
+                      <div className={styles.product}>
+                        <div className={styles.content}>
+                          <div>
+                            <p>{e.type}</p>
+                            <h3>{e.productName}</h3>
+                          </div>
+                          <div className={styles.price__layout}>
+                            <p className={styles.percent}>{e.percent}%</p>
+                            <p className={styles.price}>
+                              {e.price.toLocaleString()}원
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </Card.Body>
                   </Card>
                 ))}
               </div>
+              <Button className={styles.show__all__btn}>전체 상품 보기</Button>
             </div>
+          </section>
+          <section className={styles.coupon__section}>
+            <h2>
+              어디서든 사용 가능한
+              <br />
+              15% 쿠폰을 드려요!
+            </h2>
+            <p>쿠폰 지급 기간 : ~12월 31일까지</p>
           </section>
         </div>
       </div>
